056bf9353f9a6fff698dd4224d73d9ac6052a4e5
20huangsuqing <saewamanatsanan2012@gmail.com>
2022-10-28 17:03:27 +0700

    version 0: html5

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask , render_template,request
+
+from waitress import serve
+
+app = Flask(__name__)
+
+app.route("/")
+def index():
+
+    homepage = "<h1>主頁</h1>"
+    homepage += "<a href=/aboutnutty>MIS個人介紹</a><br><br>"
+    homepage += "<a href=/hobby>我的興趣</a><br><br>"
+    homepage += "<a href=/job>職業</a><br><br>"
+    homepage += "<a href=/plan>未來的計劃</a><br><br>"
+    return homepage
+    
+@app.route('/aboutnutty')
+def about(): 
+   return render_template('aboutnutty.html')
+
+@app.route('/hobby')
+def hobby(): 
+   return render_template('hobby.html')
+
+@app.route('/job')
+def job(): 
+   return render_template('job.html')
+
+@app.route('/plan')
+def plan(): 
+   return render_template('plan.html')
+
+if __name__ == '__main__':
+   #app.run()
+   serve(app, host='0.0.0.0', port=8080)
+ 
